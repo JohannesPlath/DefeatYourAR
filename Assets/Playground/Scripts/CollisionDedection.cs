9f4c4342e4dd824eded4eb3eb6a13d9adf3e8d8a
JohannesPlath <plath.johannes@gmail.com>
2023-03-29 02:43:01 +0200

    explosion Funtst

--- Assets/Playground/Scripts/CollisionDedection.cs
+++ Assets/Playground/Scripts/CollisionDedection.cs
@@ -7,6 +7,7 @@ public class CollisionDedection : MonoBehaviour
     private GameObject objectToDelete;
     private GameObject bullet;
     private static int counter = 0;
+    public GameObject exp;
     void OnCollisionEnter(Collision collision)
     {
        // Debug.Log("------>>>>  @ CollisionDedection +collision.gameObject.tag: " + collision.gameObject.tag);
@@ -22,18 +23,32 @@ public class CollisionDedection : MonoBehaviour
             //ChangeObjCollor(bullet);
             Destroy(objectToDelete);
             TryToDestroy(bullet);
+            
+            Explosion(bullet);
         }
         
     }
     private void TryToDestroy(GameObject ob)
         {   
-            var exp = GetComponent<ParticleSystem>();
-            exp.Play();
-            Destroy(ob, exp.duration);
             Destroy(ob);
         }
-    
-    /*private void ChangeObjCollor(GameObject ob)
+
+    private void Explosion(GameObject ob)
+    {
+        GameObject _exp = Instantiate(this.exp, transform.position, transform.rotation);
+        //var exp = GetComponent<ParticleSystem>();
+        //exp.Play();
+        Destroy(_exp, 1.5f);
+        Destroy(ob);
+        /*Explode explode = GameObject.FindGameObjectWithTag("Explode").GetComponent<Explode>();
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(ob.transform.position), out hit))
+        {
+            Instantiate(explode, hit.point, Quaternion.identity);
+        }*/
+    }
+    /*private void ChangeObjColor(GameObject ob)
         {
             var objectRd = ob.GetComponent<Renderer>();
             Debug.Log(" ----->>>>   @ ChangeColor()  renderer: " + objectRd.material);
