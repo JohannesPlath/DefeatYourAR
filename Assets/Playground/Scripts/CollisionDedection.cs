10c4efc749718c633b93e96bc0efd1511f1ec471
JohannesPlath <plath.johannes@gmail.com>
2023-03-29 03:07:23 +0200

    Zombi position otimized

--- Assets/Playground/Scripts/CollisionDedection.cs
+++ Assets/Playground/Scripts/CollisionDedection.cs
@@ -13,20 +13,15 @@ public class CollisionDedection : MonoBehaviour
        // Debug.Log("------>>>>  @ CollisionDedection +collision.gameObject.tag: " + collision.gameObject.tag);
         if (collision.gameObject.tag == "Zombie")
         {
-        
             objectToDelete = GameObject.Find(collision.gameObject.name);
-            //objectToDelete.GetComponent<Renderer>().material.color  = UnityEngine.Random.ColorHSV(0.002f, 0.2f);
             bullet = GameObject.Find("Bullet");
-            Debug.Log(" Try FInd BUllet + gameObject :   " + bullet);
+            //Debug.Log(" Try FInd BUllet + gameObject :   " + bullet);
             counter += 1; 
-            Debug.Log("Counter: " + counter);
-            //ChangeObjCollor(bullet);
-            Destroy(objectToDelete);
+            //Debug.Log("Counter: " + counter);
+            TryToDestroy(objectToDelete);
             TryToDestroy(bullet);
-            
             Explosion(bullet);
         }
-        
     }
     private void TryToDestroy(GameObject ob)
         {   
@@ -36,27 +31,9 @@ public class CollisionDedection : MonoBehaviour
     private void Explosion(GameObject ob)
     {
         GameObject _exp = Instantiate(this.exp, transform.position, transform.rotation);
-        //var exp = GetComponent<ParticleSystem>();
-        //exp.Play();
         Destroy(_exp, 1.5f);
         Destroy(ob);
-        /*Explode explode = GameObject.FindGameObjectWithTag("Explode").GetComponent<Explode>();
-        RaycastHit hit;
-
-        if (Physics.Raycast(Camera.main.ScreenPointToRay(ob.transform.position), out hit))
-        {
-            Instantiate(explode, hit.point, Quaternion.identity);
-        }*/
+        
     }
-    /*private void ChangeObjColor(GameObject ob)
-        {
-            var objectRd = ob.GetComponent<Renderer>();
-            Debug.Log(" ----->>>>   @ ChangeColor()  renderer: " + objectRd.material);
-            objectRd.material.SetColor("_Color", Color.red);
-            //gameObject.GetComponent<Renderer>().material.color  = UnityEngine.Random.ColorHSV(0.002f, 0.2f);
-            
-
-            
-        }*/
-
+    
 }
